9a7cb29b50e8021afb871c216b47b71d53b69ea5
josemasar <josemanuelsarrion@gmail.com>
2021-03-13 23:05:55 +0100

    Expo-Image-Picker

--- App.js
+++ App.js
@@ -1,24 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if(pickerResult.cancelled === true ){
+      return;
+    }
+
+    setSelectedImage({localUri: pickerResult.uri})
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-      <Image source={{uri:"https://picsum.photos/200"}} style={styles.image}/>
+      <Text style={styles.title}>Select an image from your Phone!</Text>
+      <Image source={{uri: selectedImage !== null ? selectedImage.localUri : "https://picsum.photos/200"}} style={styles.image}/>
       <Button 
         color="blue"
-        title="click me"
+        title="This is only a button"
         accessibilityLabel="you will learn more"
         onPress={ () => Alert.alert("Funciona?")}
       />
       <TouchableOpacity
       style={styles.button}
       accessibilityLabel="very customizable button"
-      onPress={ () => Alert.alert("Este es mejor!")}
+      onPress={openImagePickerAsync}
       >
-        <Text style={styles.buttonText}>New button</Text>
+        <Text style={styles.buttonText}>New image</Text>
       </TouchableOpacity>
 
       <StatusBar style="auto" />
@@ -39,7 +60,8 @@ const styles = StyleSheet.create({
   image: {
     height: 200,
     width: 200,
-    borderRadius: 100
+    borderRadius: 100,
+    marginTop: 5,
   },
   button:{
     borderRadius:2,
